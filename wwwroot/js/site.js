e2131eb35dc1f878b33f127830ca45e9c6931807
BRYAN EMILIANO GITMAN <46701940@ort>
2022-09-15 15:57:27 -0300

    Falta hacer function mostrarInfo y los controllers de todas

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,74 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function MostrarTemporadas(IdS)
+{
+    $.ajax(
+        {
+            type: 'POST',
+            dataType: 'JSON',
+            url: '/Home/VerTemporadasAjax',
+            data: { IdSerie: IdS },
+            success:
+                function (response)
+                {
+                    $("#NombreSerie").html("Temporadas de la serie @ViewBag.listadoSeries[" + IdS + "].Nombre");
+                    $("#ContenidoSerie").html("@{foreach(Temporadas temporada in " + response + "){<p>Temporada @temporada.NumeroTemporada - @temporada.TituloTemporada</p>}}");
+                },
+            error:
+                function (response)
+                {
+                    $("#NombreSerie").html("Temporadas de la serie @ViewBag.listadoSeries[" + IdS + "].Nombre");
+                    $("#ContenidoSerie").html("No se encontraron temporadas de esta serie");
+                }
+        }
+    );
+}
+
+function MostrarActores(IdS)
+{
+    $.ajax(
+        {
+            type: 'POST',
+            dataType: 'JSON',
+            url: '/Home/VerActoresAjax',
+            data: { IdSerie: IdS },
+            success:
+                function (response)
+                {
+                    $("#NombreSerie").html("Actores de la serie @ViewBag.listadoSeries[" + IdS + "].Nombre");
+                    $("#ContenidoSerie").html("@{foreach(Actores actor in " + response + "){<p>@actor.Nombre</p>}}");
+                },
+            error:
+                function (response)
+                {
+                    $("#NombreSerie").html("Actores de la serie @ViewBag.listadoSeries[" + IdS + "].Nombre");
+                    $("#ContenidoSerie").html("No se encontraron actores de esta serie");
+                }
+        }
+    );
+}
+
+function MostrarInfo(IdS)
+{
+    $.ajax(
+        {
+            type: 'POST',
+            dataType: 'JSON',
+            url: '/Home/VerInfoSerieAjax',
+            data: { IdSerie: IdS },
+            success:
+                function (response)
+                {
+                    $("#NombreSerie").html("Actores de la serie @ViewBag.listadoSeries[" + IdS + "].Nombre");
+                    $("#ContenidoSerie").html("@{foreach(Actores actor in " + response + "){<p>@actor.Nombre</p>}}");
+                },
+            error:
+                function (response)
+                {
+                    $("#NombreSerie").html("Actores de la serie @ViewBag.listadoSeries[" + IdS + "].Nombre");
+                    $("#ContenidoSerie").html("No se encontraron actores de esta serie");
+                }
+        }
+    );
+}
